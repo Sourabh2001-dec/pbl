94938b68a52029b66432cb2d0583eb2405b9d8ea
Sourabh Kumbhar <kumsou6@gmail.com>
2020-02-15 13:55:18 +0530

    added session navbar file

---
+++ session_navbar.php
@@ -0,0 +1,30 @@
+<div class="navbar clearfix">
+
+    <div class="logo">
+        <img src="resources\img\logo2x.png" alt="">
+    </div>
+
+    <ul class="navbar_menu">
+        <li><a href="#">Create Post</a></li>
+        <li><a href="login.php">Sign In</a></li>
+    </ul>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
